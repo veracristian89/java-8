588bf4cd49cd3622ba7c729ca17353ae2dcaea07
Cristian <veracristian89@gmail.com>
2022-06-23 16:41:04 -0300

    fin clase8

--- src/alura/Clase7.java
+++ src/alura/Clase7.java
@@ -25,28 +25,3 @@ public class Clase7 {
 	}	
 }
 
-class Curso {
-	private String nombre;
-	private int horas;
-	
-	public Curso(String nombre, int horas) {
-		this.nombre = nombre;
-		this.horas = horas;
-		
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public int getHoras() {
-		return horas;
-	}
-
-	@Override
-	public String toString() {
-		return "nombre= " + nombre + ", horas= " + horas;
-	}
-	
-	
-}
